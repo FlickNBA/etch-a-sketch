1eb3227a110735b3fe65006d61fcb29c5be580ac
FlickNBA <flicknba@icloud.com>
2023-02-27 15:43:51 +0100

    Finished, not wasting time on extras

--- main.js
+++ main.js
@@ -14,19 +14,12 @@ const fitSquare = () => {
   }
 };
 
-fitSquare();
-
-window.addEventListener("resize", fitSquare);
-
 const setAndFillSquare = (n) => {
+  squareDiv.innerHTML = "";
   squareDiv.style["grid-template-columns"] = `repeat(${n}, 1fr)`;
   squareDiv.style["grid-template-rows"] = `repeat(${n}, 1fr)`;
 
   // CSS for end of first row and beginning of last one - I do this using JS to learn, I know setting it with CSS is easier.
-
-  // document.querySelector("#square div:nth-child(16)").style[
-  //   "background-color"
-  // ] = "red";
 
   let newSquares = [];
   for (let i = 0; i < n * n; i++) {
@@ -39,8 +32,39 @@ const setAndFillSquare = (n) => {
   });
 
   document.querySelector(`#square div:nth-child(${n})`).style["border-top-right-radius"] = "0.5rem";
-
   document.querySelector(`#square div:nth-child(${n * (n - 1) + 1})`).style["border-bottom-left-radius"] = "0.5rem";
+
+  addListeners();
 };
 
-setAndFillSquare(16);
+let currentColor = "skyblue";
+
+const addListeners = () => {
+  squareDiv.onselectstart = () => {
+    return false;
+  };
+  let allSquares = document.querySelectorAll("#square div");
+  allSquares.forEach((singleSquare) => {
+    singleSquare.addEventListener("mousemove", (e) => {
+      // e.preventDefault();
+      // e.stopPropagation();
+      console.log(`Mouse move => ${singleSquare}`);
+      if (e.buttons == 1) {
+        // e.preventDefault();
+        // e.stopPropagation();
+        console.log(`Mouse down => ${singleSquare}`);
+        singleSquare.style["background-color"] = currentColor;
+      }
+    });
+    // singleSquare.addEventListener("mouseleave", () => {
+    //   console.log(`Mouse left => ${singleSquare}`);
+    //   singleSquare.style["background-color"] = "gray";
+    // });
+  });
+};
+
+fitSquare();
+
+window.addEventListener("resize", fitSquare);
+
+setAndFillSquare(64);
